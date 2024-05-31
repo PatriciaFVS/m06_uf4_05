b308627c2e32b29cf5c74c79e7967e01c5beeb0d
patricia <pvalenci22@jaumebalmes.net>
2024-05-31 14:58:26 +0200

    fet correctament amb asyn y await

--- promises.js
+++ promises.js
@@ -1,57 +1,78 @@
-function divisibleperDos(num) {
-    return new Promise((resolve, reject) => {
-        if (num % 2 === 0) {
-            resolve(`${num} es divisible por 2.`);
-        } else {
-            reject(`${num} no es divisible por 2.`);
-        }
-    });
-   
-} 
-divisibleperDos(8).then(console.log).catch(console.log);
-divisibleperDos(5).then(console.log).catch(console.log);
-
-const comprovaRang = (valor) => {
-    return new Promise((resolve, reject) => {
-        if (valor >= 0 && valor <= 10) {
-            resolve(`${valor} está entre 0 y 10.`);
-        } else {
-            reject(`${valor} no está entre 0 y 10.`);
-        }
-    });
+async function divisiblePerDos(num) {
+    try {
+        const result = await new Promise((resolve, reject) => {
+            if (num % 2 === 0) {
+                resolve(`${num} es divisible por 2.`);
+            } else {
+                reject(`${num} no es divisible por 2.`);
+            }
+        });
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-comprovaRang(2).then(console.log).catch(console.log);
-comprovaRang(15).then(console.log).catch(console.log);
-comprovaRang(-10).then(console.log).catch(console.log);
+
+divisiblePerDos(8);
+divisiblePerDos(5);
+
+async function comprovaRang(valor) {
+    try {
+        const result = await new Promise((resolve, reject) => {
+            if (valor >= 0 && valor <= 10) {
+                resolve(`${valor} está entre 0 y 10.`);
+            } else {
+                reject(`${valor} no está entre 0 y 10.`);
+            }
+        });
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+comprovaRang(2);
+comprovaRang(15);
+comprovaRang(-10);
 
 let arrVocals = ["a", "e", "i", "o", "u"];
 
-function esVocal(lletra) {
-    return new Promise((resolve, reject) => {
-        if (arrVocals.includes(lletra.toLowerCase())) {
-            resolve(`${lletra} es una vocal.`);
-        } else {
-            reject(`${lletra} no es una vocal.`);
-        }
-    });
+async function esVocal(lletra) {
+    try {
+        const result = await new Promise((resolve, reject) => {
+            if (arrVocals.includes(lletra.toLowerCase())) {
+                resolve(`${lletra} es una vocal.`);
+            } else {
+                reject(`${lletra} no es una vocal.`);
+            }
+        });
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-esVocal("a").then(console.log).catch(console.log);
-esVocal("b").then(console.log).catch(console.log);
-esVocal("u").then(console.log).catch(console.log);
-esVocal("ñ").then(console.log).catch(console.log);
 
+esVocal("a");
+esVocal("b");
+esVocal("u");
+esVocal("ñ");
 
-function divisio(primerVal, segonVal) {
-    return new Promise((resolve, reject) => {
-        if (segonVal !== 0) {
-            resolve(`El resultado de la división es ${primerVal /segonVal}.`);
-        } else {
-            reject("Error: División por cero.");
-        }
-    });
+async function divisio(primerVal, segonVal) {
+    try {
+        const result = await new Promise((resolve, reject) => {
+            if (segonVal !== 0) {
+                resolve(`El resultado de la división es ${primerVal / segonVal}.`);
+            } else {
+                reject("Error: División por cero.");
+            }
+        });
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-divisio(10, 2).then(console.log).catch(console.log);
-divisio(7, 0).then(console.log).catch(console.log);
+divisio(10, 2);
+divisio(7, 0);
